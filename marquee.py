40ab46ea53e9e4c33abd14bce7f37fd20097fdbd
Juan Manuel Schillaci <jmschillaci@gmail.com>
2024-06-21 14:38:54 -0300

    Added clear button

--- marquee.py
+++ marquee.py
@@ -13,17 +13,24 @@ def read_fifo(fifo_path, text_widget):
             else:
                 time.sleep(0.1)  # Evita un loop ocupado
 
+def clear_text(text_widget):
+    text_widget.delete('1.0', tk.END)  # Borra todo el contenido del Text widget
+
+
 def main():
     fifo_path = 'myfifo'  # Asegúrate de que este path sea correcto y que el FIFO exista
 
     # Crear una ventana de Tkinter
     root = tk.Tk()
-    root.title("FIFO Reader")
-
+    root.title("No tengo wifi")
+    root.geometry('800x600')  # Define el tamaño de la ventana (ancho x alto)
     # Crear un Text widget
-    font_settings = ('Operator Mono Lig', 36)
+    font_settings = ('Terminus', 36)
     text_widget = tk.Text(root, wrap='word', font=font_settings, bg='black', fg='green')
     text_widget.pack(expand=True, fill='both')
+    # Crear un botón para limpiar el Text widget
+    clear_button = tk.Button(root, text="Clear", command=lambda: clear_text(text_widget))
+    clear_button.place(relx=1.0, rely=0.0, anchor='ne')  # Posicionar el botón en la esquina superior derecha
 
     # Leer datos del FIFO y mostrarlos en el Text widget
     reader_thread = threading.Thread(target=read_fifo, args=(fifo_path, text_widget))
