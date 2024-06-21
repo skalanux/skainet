e4fa5659201997a3fd59f39d3afb28495cc4b170
Juan Manuel Schillaci <jmschillaci@gmail.com>
2024-06-21 16:01:32 -0300

    Added frontend console

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 import time
 from morse_equivs import equivs
 
+import logging
+
 def is_light_on(frame, threshold=250, min_brightness_area=23500):
     # Convertir el frame a escala de grises
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -23,24 +25,6 @@ def is_light_on(frame, threshold=250, min_brightness_area=23500):
     
     return light_on, bright_area, thresh
 
-# Capturar el video desde la cámara
-cap = cv2.VideoCapture(0)  # Cambiar el índice si hay más de una cámara
-
-last_checked_time = time.time()
-check_interval =  0.01 # Intervalo de tiempo para verificar el estado de la luz en segundos
-
-cant_lights = 0
-cant_darks = 0
-symbols = ''
-
-palabra = ''
-
-INTERVAL_BETWEEN_WORDS = (13,20) # OK
-INTERVAL_BETWEEN_SYMBOLS = (6,12)
-
-LIGHT_INTERVAL_DOT = (1,7)
-LIGHT_INTERVAL_DASH = (8,20)
-
 def print2(value):
     global palabra
     palabra+=value
@@ -52,71 +36,88 @@ def write_to_fifo(char):
             fifo.write(char)
             fifo.flush()  # Asegúrate de que los datos se escriban inmediatamente
 
-print_space = False
-print_symbol = False
-keep_scanning = True
 
-time.sleep(2)
-print("Decoding...")
+def write_to_queue(morse_queue, char):
+    morse_queue.put(char)
 
-while keep_scanning:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    
-    light_on, bright_area, thresh = is_light_on(frame)
 
+def scan(morse_queue):
+    # Capturar el video desde la cámara
+    cap = cv2.VideoCapture(0)  # Cambiar el índice si hay más de una cámara
+
+    last_checked_time = time.time()
+    check_interval =  0.01 # Intervalo de tiempo para verificar el estado de la luz en segundos
+
+    cant_lights = 0
+    cant_darks = 0
+    symbols = ''
+
+    INTERVAL_BETWEEN_WORDS = (13,20) # OK
+    INTERVAL_BETWEEN_SYMBOLS = (6,12)
+
+    LIGHT_INTERVAL_DOT = (1,7)
+    LIGHT_INTERVAL_DASH = (8,20)
 
     print_space = False
     print_symbol = False
 
-    if light_on:
-        cant_lights+=1
-        if cant_darks>0:
-            if cant_darks in range(*INTERVAL_BETWEEN_WORDS):
-                print_space = True
-                print_symbol = True
-            elif cant_darks in range(*INTERVAL_BETWEEN_SYMBOLS):
-                print_symbol = True
+    print("Decoding...")
 
-        cant_darks=0
-    else:
-        cant_darks+=1
-        if cant_lights>0:
-            if cant_lights in range(*LIGHT_INTERVAL_DOT):
-                symbols+='.'
-            elif cant_lights in range(*LIGHT_INTERVAL_DASH):
-                symbols+='-'
-
-        cant_lights=0
-   
-        if cant_darks > INTERVAL_BETWEEN_WORDS[1] and symbols:
-            print_symbol = True
-            #keep_scanning = False
-
-    if print_symbol:
-        #print(symbols, equivs.get(symbols))
-        #print(equivs.get(symbols))
-        letter=equivs.get(symbols)
-        #sys.stdout.write(letter)
-        write_to_fifo(letter)
-        symbols=''
-    if print_space:
-        #print(" \\ ")
-        #print(" ")
-        #sys.stdout.write(' ')
-        write_to_fifo(' ')
-        symbols=''
-    #sys.stdout.flush()
-    # Mostrar el frame original y el frame con el umbral aplicado
-        #cv2.imshow("Frame", frame)
-        #cv2.imshow("Threshold", thresh)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
         
-    # Salir del loop si se presiona la tecla 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        light_on, bright_area, thresh = is_light_on(frame)
 
-# Liberar el objeto de captura y cerrar todas las ventanas
-cap.release()
-cv2.destroyAllWindows()
+
+        print_space = False
+        print_symbol = False
+
+        if light_on:
+            cant_lights+=1
+            if cant_darks>0:
+                if cant_darks in range(*INTERVAL_BETWEEN_WORDS):
+                    print_space = True
+                    print_symbol = True
+                elif cant_darks in range(*INTERVAL_BETWEEN_SYMBOLS):
+                    print_symbol = True
+
+            cant_darks=0
+        else:
+            cant_darks+=1
+            if cant_lights>0:
+                if cant_lights in range(*LIGHT_INTERVAL_DOT):
+                    symbols+='.'
+                elif cant_lights in range(*LIGHT_INTERVAL_DASH):
+                    symbols+='-'
+
+            cant_lights=0
+       
+            if cant_darks > INTERVAL_BETWEEN_WORDS[1] and symbols:
+                print_symbol = True
+                #keep_scanning = False
+
+        if print_symbol:
+            logging.debug(f"{symbols}: {equivs.get(symbols)}")
+            #print(equivs.get(symbols))
+            letter=equivs.get(symbols)
+            write_to_queue(morse_queue, letter)
+            symbols=''
+        if print_space:
+            #print(" \\ ")
+            #print(" ")
+            write_to_queue(morse_queue, ' ')
+            symbols=''
+        # Mostrar el frame original y el frame con el umbral aplicado
+            #cv2.imshow("Frame", frame)
+            #cv2.imshow("Threshold", thresh)
+            
+        # Salir del loop si se presiona la tecla 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Liberar el objeto de captura y cerrar todas las ventanas
+    cap.release()
+    cv2.destroyAllWindows()
 
