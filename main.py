0a15296744f3af5df755fbfa10e4b50e9a84b8df
Juan Manuel Schillaci <jmschillaci@gmail.com>
2024-06-21 20:16:39 -0300

    Improved interface

--- main.py
+++ main.py
@@ -24,14 +24,19 @@ def worker(morse_queue, text_widget):
         item = morse_queue.get()
 
         if item:
-            text_widget.insert(tk.END, item)
+            delete_last_char(text_widget)
+            text_widget.insert(tk.END, item+'_')
             #text_widget.see(tk.END)
         morse_queue.task_done()
 
+def delete_last_char(text_widget):
+    text_value = text_widget.get("1.0", tk.END)  # Obtener todo el texto
+    if len(text_value) > 1:  # Verificar que haya más de un carácter (incluido el salto de línea final)
+        text_widget.delete("end-2c")
 
 def clear_text(text_widget):
     text_widget.delete('1.0', tk.END)  # Borra todo el contenido del Text widget
-    text_widget.insert(tk.END, ' > ')
+    text_widget.insert(tk.END, ' >_')
 
 
 def show():
@@ -42,7 +47,7 @@ def show():
     font_settings = ('Terminus', 36)
     text_widget = tk.Text(root, wrap='word', font=font_settings, bg='black', fg='green')
     text_widget.pack(expand=True, fill='both')
-    text_widget.insert(tk.END, ' > ')
+    text_widget.insert(tk.END, ' >_')
     run_read_button = tk.Button(root, text="Start/Stop", command=lambda: toggle_decoder())
     run_read_button.place(relx=0.4, rely=0.8, anchor='ne')  # Posicionar el botón en la esquina superior derecha
 
